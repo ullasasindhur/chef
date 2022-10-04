883d3babce08a3f2b9453ff5649eb8b513a98018
ullasasindhur <poojithullasa@gmail.com>
2022-10-04 17:38:19 +0530

    Added inputs to the test cases

--- cookbooks/ullasasindhur_apache/test/integration/apache/controls/test.rb
+++ cookbooks/ullasasindhur_apache/test/integration/apache/controls/test.rb
@@ -1,15 +1,17 @@
-title 'Testing '
+title 'Testing of cookbook ullasasindhur_apache'
+
+input('package_name', value: 'httpd')
 
 control 'apache' do
-  impact 0.7
+  impact 0
   title 'Testing the ullasasindhur_apache cookbook'
   desc 'Checking whether package is installed, service in runnning and port is configured correctly'
-  describe service('apache2') do
+  describe service(input('package_name')) do
     it { should be_installed }
     it { should be_enabled }
     it { should be_running }
   end
-  describe port(8080) do
+  describe port(input('port_apache')) do
     it { should be_listening }
   end
 end
