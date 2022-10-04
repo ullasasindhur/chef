99a2bbbda2a7de0b4ffea342f57d39688dc02f35
ullasasindhur <poojithullasa@gmail.com>
2022-10-04 18:26:48 +0530

    added polcifyfile

---
+++ Policyfiles/Policyfile.rb
@@ -0,0 +1,19 @@
+# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile/
+
+# A name that describes what the system you're building with Chef does.
+name 'assignment'
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'recipe[ullasasindhur_apache]', 'recipe[ullasasindhur_mysql]'
+
+# Specify a custom source for a single cookbook:
+# cookbook 'example_cookbook', path: '../cookbooks/example_cookbook'
+cookbook 'ullasasindhur_apache', path: '../cookbooks/ullasasindhur_apache'
+cookbook 'ullasasindhur_mysql', path: '../cookbooks/ullasasindhur_mysql'
